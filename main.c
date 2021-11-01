f8a9034cdc16770f593d533c45b9ec20b8972571
ptoughneigh <58543031+ynot278@users.noreply.github.com>
2021-11-01 23:30:24 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX_REAL_SECONDS 3
+#define MAX_USERS 50
+
+static int seconds = MAX_REAL_SECONDS;
+const char *logFile = "ouput.txt";
+
+static void printHelp(){
+	printf("oss [-h] [-s t] [-l f]\n");
+	printf("-h Describe how the project should be run and then, terminate.\n");
+	printf("-s t Indicate how many maximum seconds before the system terminates.\n");
+	printf("-l f Specify a particular name for the log file.\n");
+}
+
+static int parseCommands(const int argc, char *const argv[]){
+	int opt;
+
+	while ((opt = getopt(argc, argv, "hs:l:")) > 0){
+		switch (opt){
+			case 'h':
+				printHelp();
+				return -1;
+			
+			case 's':
+				seconds = atoi(optarg);
+				break;
+
+			case 'l':
+				logFile = optarg;
+				break;
+
+			default:
+				printHelp();
+				return -1;
+		}
+	}
+}
+
+int main(const int argc, char *const argv[]){
+  if ((parseCommands(argc, argv) < 0)){
+		return EXIT_FAILURE;
+	}
+}
